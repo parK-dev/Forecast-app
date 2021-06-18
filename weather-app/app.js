4c95c3f147a352690db344732838ae390b388a0f
Kirk Paradis <kirk.paradis@gmail.com>
2021-06-18 16:55:33 +0900

    API Calls now functional with exceptions as well

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,3 +1,34 @@
-const key = "5a8f40114450650feb87a04c3a8d86b8";
-const url = `https//api.weatherstack.com/current?access_key=${key}&query=37.8267,-122.4233`;
+import request from 'postman-request';
+import dotenv from 'dotenv/config';
+
+// Weather
+const weatherKey = process.env.WEATHER_KEY;
+const weatherUrl = `http://api.weatherstack.com/current?access_key=${weatherKey}&query=Nagoya`;
+
+request({url: weatherUrl, json: true}, (error, response, body) => {
+  if (error) {
+    console.log('Unable to connect to weather service.');
+  } else if (body.error) {
+    console.log(`Error: ${body.error.info}`)
+  } else {
+    const wData = body.current
+    console.log(`${wData.weather_descriptions[0]}.The temperature in Nagoya is ${wData.temperature} degrees. There is ${wData.precip}% chance of precipitations.`);
+  }
+});
+
+// Geocoding
+const mapKey = process.env.MAP_KEY;
+const mapUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${mapKey}&limit=1`
+
+request({url: mapUrl, json: true}, (error, response, body) => {
+  if (error) {
+    console.log('Unable to connect to geolocation service.');
+  } else if (body.message) {
+    console.log(`Error: ${body.message}`)
+  } else {
+    const lat = body.features[0].center[1];
+    const lon = body.features[0].center[0];
+    console.log(`Latitude: ${lat} Longitude: ${lon}`);
+  }
+});
 
