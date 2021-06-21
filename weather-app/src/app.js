c60898f14ceb8d99d4255599605efbe3ec189799
Kirk Paradis <kirk.paradis@gmail.com>
2021-06-21 14:30:28 +0900

    setup templates, views and partials

--- weather-app/src/app.js
+++ weather-app/src/app.js
@@ -2,18 +2,36 @@
 import geocode from '../utils/geocode.js';
 import forecast from '../utils/forecast.js';
 import express from 'express';
-import path from 'path';
+import * as path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import hbs from 'hbs';
 
 const app = express();
-app.use(express.static(path.join(__dirname, '../public')));
+
+//Define paths for Express config
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname,'../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine views and location
+app.set('view engine', 'hbs'); // set template engine for Express
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath)); //set HTML files for routes
 
 app.get('', (req, res) => {
-  
+  res.render('index', {
+    title: 'A Weather App',
+    author: 'parK-dev'
+  });
 });
-
 app.listen(3000, () => {
   
-});
+}); // set port
 
 const address = process.argv[2];
 
