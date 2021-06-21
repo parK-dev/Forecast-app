0caa2f0c64bf5bc1bf187d8f2d5cc70558155bf5
Kirk Paradis <kirk.paradis@gmail.com>
2021-06-21 13:26:52 +0900

    Created frontend files and folders placeholders

--- weather-app/src/app.js
+++ weather-app/src/app.js
@@ -1,8 +1,22 @@
 /* eslint-disable no-undef */
 import geocode from '../utils/geocode.js';
 import forecast from '../utils/forecast.js';
+import express from 'express';
+import path from 'path';
+
+const app = express();
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('', (req, res) => {
+  
+});
+
+app.listen(3000, () => {
+  
+});
 
 const address = process.argv[2];
+
 if(!address) {
   console.log('Please provide a location.');
 } else {
